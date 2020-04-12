cdaceb14cb99333de58e0ef3b2d660a62eff2341
ErikaTozetto <63562316+ErikaTozetto@users.noreply.github.com>
2020-04-12 20:18:03 -0300

    Update PedidoResource.java

--- src/main/java/br/com/sample/api/v1/PedidoResource.java
+++ src/main/java/br/com/sample/api/v1/PedidoResource.java
@@ -1,1 +1,16 @@
+package br.com.sample.api.v1;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.springframework.hateoas.ResourceSupport;
+import org.springframework.hateoas.core.Relation;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@Relation(value="book", collectionRelation="pedidos")
+class BookResource extends ResourceSupport {
+
+    private String origem;
+    private String destino;
+    private Integer quilometro;
+}

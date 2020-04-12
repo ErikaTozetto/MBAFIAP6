a44a0ded6791b4043ab6fe30507e91571d2dd298
ErikaTozetto <63562316+ErikaTozetto@users.noreply.github.com>
2020-04-12 20:13:59 -0300

    Update Pedido.java

--- src/main/java/br/com/sample/domain/orm/Pedido.java
+++ src/main/java/br/com/sample/domain/orm/Pedido.java
@@ -1,1 +1,25 @@
+package br.com.sample.domain.orm;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Pedido {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String destino;
+    private String origem;
+    private Integer quilometro;
+}

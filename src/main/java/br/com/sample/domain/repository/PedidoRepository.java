2e0b2aadeb7cbb5f9879fb4af9e908d3ef841789
ErikaTozetto <63562316+ErikaTozetto@users.noreply.github.com>
2020-04-12 20:11:07 -0300

    Update PedidoRepository.java

--- src/main/java/br/com/sample/domain/repository/PedidoRepository.java
+++ src/main/java/br/com/sample/domain/repository/PedidoRepository.java
@@ -1,1 +1,8 @@
+package br.com.sample.domain.repository;
 
+import br.com.sample.domain.orm.Pedido;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PedidoRepository extends CrudRepository<Pedido, Integer> {}

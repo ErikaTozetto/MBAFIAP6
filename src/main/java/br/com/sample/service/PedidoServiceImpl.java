cc9b4ddc0a110bc9640d250b6420e36a1dd50f65
ErikaTozetto <63562316+ErikaTozetto@users.noreply.github.com>
2020-04-12 20:09:10 -0300

    Update PedidoServiceImpl.java

--- src/main/java/br/com/sample/service/PedidoServiceImpl.java
+++ src/main/java/br/com/sample/service/PedidoServiceImpl.java
@@ -1,1 +1,34 @@
+package br.com.sample.service;
 
+import br.com.sample.domain.orm.Pedido;
+import br.com.sample.domain.repository.PedidoRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import static java.util.Objects.isNull;
+
+@Service
+@AllArgsConstructor
+public class PedidoServiceImpl implements PedidoService {
+
+    private final PedidoRepository pedidoRepository;
+
+    @Override
+    public Pedido getById(Integer id) {
+        Pedido Pedido = pedidoRepository.findOne(id);
+        if(isNull(pedido)) {
+            throw new RuntimeException("Pedido não encontrado!");
+        }
+        return pedido;
+    }
+
+    @Override
+    public Pedido save(Pedido pedido) {
+        if (pedido.getQuilometro() < 1)
+        {
+          throw new RuntimeException("Vá a pé, exercite-se!!... viagens abaixo de 1 km não são realizadas!");
+        }
+       
+        return pedidoRepository.save(pedido);
+    }
+}
